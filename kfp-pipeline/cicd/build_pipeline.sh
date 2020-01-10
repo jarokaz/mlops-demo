aa2f3534e6ef6e8e2f10639414ad387819bbb8bc
jarokaz <jarokaz@users.no-reply.github.com>
2020-01-10 02:42:11 +0000

    Updates

--- kfp-pipeline/cicd/build_pipeline.sh
+++ kfp-pipeline/cicd/build_pipeline.sh
@@ -17,17 +17,17 @@
 # the pipelines and pipelines components 
 
 SUBSTITUTIONS=\
-_INVERTING_PROXY_HOST=[YOUR_INVERTING_PROXY_HOST],\
+_INVERTING_PROXY_HOST=1ff32660e53f5d89-dot-us-central1.notebooks.googleusercontent.com,\
 _TRAINER_IMAGE_NAME=trainer_image,\
 _BASE_IMAGE_NAME=base_image,\
-TAG_NAME=manual,\
-_PIPELINE_FOLDER=Lab-11-KFP-CAIP/pipelines,\
+TAG_NAME=test,\
+_PIPELINE_FOLDER=kfp-pipeline/dsl,\
 _PIPELINE_DSL=covertype_training_pipeline.py,\
 _PIPELINE_PACKAGE=covertype_training_pipeline.yaml,\
-_PIPELINE_NAME=covertype_training_deployment,\
+_PIPELINE_NAME=covertype_classifier_training,\
 _RUNTIME_VERSION=1.14,\
 _PYTHON_VERSION=3.5,\
-_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.1.38/components/gcp/
+_COMPONENT_URL_SEARCH_PREFIX=https://raw.githubusercontent.com/kubeflow/pipelines/0.1.36/components/gcp/
 
 
 gcloud builds submit ../.. --config cloudbuild.yaml --substitutions $SUBSTITUTIONS
